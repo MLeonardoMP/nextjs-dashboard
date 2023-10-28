7db159f5a009c5dd7e33913a59b1b1a17ee2795d
Leonardo Mosquera <mlmosquerap@gmail.com>
2023-10-27 23:18:25 -0500

    Chapter 7 completed

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -21,11 +21,15 @@ export const formatDateToLocal = (
   return formatter.format(date);
 };
 
+
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
+
   const yAxisLabels = [];
+
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
+ 
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
   for (let i = topLabel; i >= 0; i -= 1000) {
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,13 +2,15 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-export default async function LatestInvoices() {
+import { LatestInvoice } from '@/app/lib/definitions';
+export default async function LatestInvoices({latestInvoices,}: {latestInvoices: LatestInvoice[];}) {
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
-      {/* <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
@@ -24,6 +26,7 @@ export default async function LatestInvoices() {
                 <div className="flex items-center">
                   <Image
                     src={invoice.image_url}
+                    alt='invoice image'
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
@@ -50,7 +53,7 @@ export default async function LatestInvoices() {
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
