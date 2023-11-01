bad12603c43b4ccf15738108a3dd98bad931a6da
Leonardo Mosquera <mlmosquerap@gmail.com>
2023-11-01 15:42:46 -0500

    Chapter 11 completed

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,31 +2,37 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-// import { useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  // const searchParams = useSearchParams();
-  // const { replace } = useRouter();
-  // const pathname = usePathname();
+  
 
-  // const handleSearch = useDebouncedCallback((term) => {
-  //   console.log(`Searching... ${term}`);
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
 
-  //   const params = new URLSearchParams(searchParams);
+  
 
-  //   params.set('page', '1');
 
-  //   if (term) {
-  //     params.set('query', term);
-  //   } else {
-  //     params.delete('query');
-  //   }
-  //   replace(`${pathname}?${params.toString()}`);
-  // }, 300);
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
-      {/* <label htmlFor="search" className="sr-only">
+      <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
@@ -37,7 +43,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
